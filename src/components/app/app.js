e4b4238ca3bd030874853a2e6b6e506ec03f3bb7
Alina <ms.sciurus@gmail.com>
2024-10-03 20:10:45 +0300

    immutation

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,30 +8,60 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-function App() {
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {name: 'John C.', salary: 800, increase: false, id: 1},
+                {name: 'Alex M.', salary: 3000, increase: true, id: 2},
+                {name: 'Carl W.', salary: 5000, increase: false, id: 3}
+            ]
+        }
+        this.maxId = 4;
+    }
 
-  const data = [
-    {name: 'Mike', salary: 600, increase: false, id: 1},
-    {name: 'Luke', salary: 700, increase: false, id: 2},
-    {name:'Anakin', salary: 1, increase: true, id: 3},
-    {name:'Oleg', salary: 0.111, increase: false, id: 4},
-    {name:'Oleg', salary: 0.111, increase: true, id: 5},
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            return {
+                data: data.filter(item => item.id !== id)
+            }
+        })
+    }
 
-  ];
+    // Да, пока могут добавляться пустые пользователи. Мы это еще исправим
+    addItem = (name, salary) => {
+        const newItem = {
+            name, 
+            salary,
+            increase: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
+            }
+        });
+    }
 
-  return (
-    <div className="app">
-        <AppInfo />
-
-        <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
-        </div>
-        
-        <EmployeesList data={data}/>
-        <EmployeesAddForm/>
-    </div>
-  );
+    render() {
+        return (
+            <div className="app">
+                <AppInfo />
+    
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+                
+                <EmployeesList 
+                    data={this.state.data}
+                    onDelete={this.deleteItem}/>
+                <EmployeesAddForm onAdd={this.addItem}/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -2,12 +2,12 @@ import { Component } from 'react';
 
 import './employees-list-item.css';
 
-class EmployeesListItem extends Component {
+class EmployeesListItem extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            increase: false
-
+            increase: false,
+            rise: false
         }
     }
 
@@ -24,13 +24,14 @@ class EmployeesListItem extends Component {
     }
 
     render() {
-        const {name, salary} = this.props;
+        const {name, salary, onDelete} = this.props;
         const {increase, rise} = this.state;
-        let classNames = 'list-group-item d-flex justify-content-between'
-        if(increase) {
+
+        let classNames = "list-group-item d-flex justify-content-between";
+        if (increase) {
             classNames += ' increase';
         }
-        if(rise) {
+        if (rise) {
             classNames += ' like';
         }
     
@@ -40,13 +41,14 @@ class EmployeesListItem extends Component {
                 <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
                 <div className='d-flex justify-content-center align-items-center'>
                     <button type="button"
-                        className="btn-cookie btn-sm"
+                        className="btn-cookie btn-sm "
                         onClick={this.onIncrease}>
                         <i className="fas fa-cookie"></i>
                     </button>
     
                     <button type="button"
-                            className="btn-trash btn-sm ">
+                            className="btn-trash btn-sm "
+                            onClick={onDelete}>
                         <i className="fas fa-trash"></i>
                     </button>
                     <i className="fas fa-star"></i>
@@ -54,7 +56,6 @@ class EmployeesListItem extends Component {
             </li>
         )
     }
-   
 }
 
 export default EmployeesListItem;
